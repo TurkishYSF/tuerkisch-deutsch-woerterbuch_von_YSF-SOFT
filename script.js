cded2e3fc0981934f2cbce8043b25cfc23dd6550
TurkishYSF <77483495+TurkishYSF@users.noreply.github.com>
2023-12-30 19:48:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,331 @@
+var datas = [
+    {
+        titel: "das Alphabet",
+        untertitel: "(alfa'be:t), Substantiv, Neutrum, -e, von kirchenlateinisch alphabētum",
+        bedeutungen: "alfabe, abece",
+        beispiele: "Die deutsche Schrift verwendet das lateinische <span class='vurgu'>Alphabet</span>.",
+        sprache: "de"
+    },
+    {
+        titel: "das Niveau",
+        untertitel: "(ni'vo:), Substantiv, Neutrum, -s, von französisch niveau",
+        bedeutungen: "düzey, seviye",
+        sprache: "de"
+    },
+    {
+        titel: "das Thema",
+        untertitel: "('te:ma), Substantiv, Neutrum, -n, von lateinisch thema",
+        bedeutungen: "tema, konu",
+        sprache: "de"
+    },
+    {
+        titel: "der Aal",
+        untertitel: "(a:l), Substantiv, männlich, -e",
+        bedeutungen: "yılan balığı",
+        beispiele: "Er hatte einen sehr großen <span class='vurgu'>Aal</span> gefangen.",
+        sprache: "de"
+    },
+    {
+        titel: "der Abfall",
+        untertitel: "('apˌfal), Substantiv, männlich, Abfälle",
+        bedeutungen: "çöp, atık",
+        beispiele: "Kannst du bitte <span class='vurgu'>den Abfall</span> nach unten bringen?",
+        sprache: "de"
+    },
+    {
+        titel: "der Januar",
+        untertitel: "('janua:ɐ̯), Substantiv, männlich, -e, von lateinisch Iānuārius",
+        bedeutungen: "ocak (ayı)",
+        beispiele: "<span class='vurgu'>Der Januar</span> ist der erste Monat im Jahr. (<span class='vurgu'>Ocak (ayı)</span> yılın ilk ayıdır.)",
+        sprache: "de"
+    },
+    {
+        titel: "der Klimawandel",
+        untertitel: "('kli:maˌvandl̩), Substantiv, männlich, aus den Substantiven Klima und Wandel",
+        bedeutungen: "iklim değişikliği",
+        beispiele: "Allgemein bedeutet der Klimawandel die Veränderungen des Erdklimas.",
+        sprache: "de"
+    },
+    {
+        titel: "der Mann",
+        untertitel: "(man), Substantiv, männlich, Männer",
+        bedeutungen: "adam, er, erkek",
+        beispiele: "Er ist ein kluger <span class='vurgu'>Mann</span>.",
+        sprache: "de"
+    },
+    {
+        titel: "başlık",
+        untertitel: "ayrıntı",
+        bedeutungen: "anlam",
+        beispiele: "örnek",
+        sprache: "de"
+    },
+    {
+        titel: "der Mensch",
+        untertitel: "(mɛnʃ), Substantiv, männlich, -en",
+        bedeutungen: "insan, kişi",
+        sprache: "de"
+    },
+    {
+        titel: "der Waldbrand",
+        untertitel: "(ˈvaltˌbʁant), Substantiv, männlich, Waldbrände",
+        bedeutungen: "orman yangını",
+        beispiele: "Im Unterschied zu den vergangenen Jahren sind Brandenburgs Forste bislang von größeren <span class='vurgu'>Waldbränden</span> weitgehend verschont. 164 überwiegend kleinere <span class='vurgu'>Waldbrände</span> auf einer Gesamtfläche von 44 Hektar registrierte das Umweltministerium in der ersten Jahreshälfte.",
+        sprache: "de"
+    },
+    {
+        titel: "der Vulkanausbruch",
+        untertitel: "(vʊlˈkaːnʔaʊ̯sˌbʁʊx), Substantiv, männlich, Vulkanausbrüche",
+        bedeutungen: "yanardağ püskürmesi",
+        beispiele: "Bei <span class='vurgu'>dem Vulkanausbruch</span> starben keine Menschen, weil sie rechtzeitig evakuiert wurden.",
+        sprache: "de"
+    },
+    {
+        titel: "die Bestellung",
+        untertitel: "(bəˈʃtɛlʊŋ), Substantiv, weiblich, -en",
+        bedeutungen: "sipariş",
+        beispiele: "Ich habe vor zwei Wochen eine <span class='vurgu'>Bestellung</span> über 2000 paar Socken aufgegeben. (İki hafta önce 2000 çift çorap siparişi verdim.)",
+        sprache: "de"
+    },
+    {
+        titel: "die Luft",
+        untertitel: "(lʊft), Substantiv, weiblich, Lüfte",
+        bedeutungen: "hava",
+        beispiele: "Menschen und viele Tiere beziehen den lebensnotwendigen Sauerstoff aus <span class='vurgu'>der Luft</span>, einige Tiere aus dem Wasser. (İnsanlar ve birçok hayvan hayatta kalmak için gereksinim duydukları oksijeni <span class='vurgu'>havadan</span>, bazı hayvanlar ise sudan elde eder.)",
+        sprache: "de"
+    },
+    {
+        titel: "die Luftverschmutzung",
+        untertitel: "(ˈlʊftfɛɐ̯ˌʃmʊt͡sʊŋ), Substantiv, weiblich, -en",
+        bedeutungen: "hava kirliliği",
+        beispiele: "<span class='vurgu'>Luftverschmutzung</span> sollte unter hohe Strafen gestellt werden.",
+        sprache: "de"
+    },
+    {
+        titel: "die Türkei",
+        untertitel: "(tʏʁˈkaɪ̯), Substantiv, weiblich",
+        bedeutungen: "Türkiye",
+        beispiele: "Ich komme aus <span class='vurgu'>der Türkei</span>. (Ben <span class='vurgu'>Türkiye</span>'den gelirim.)",
+        sprache: "de"
+    },
+    {
+        titel: "abändern",
+        untertitel: "ab|ändern, (ˈapˌʔɛndɐn), transitives Verb, haben abgeändert",
+        bedeutungen: "değiştirmek",
+        beispiele: "Ich <span class='vurgu'>ändere</span> den Rock <span class='vurgu'>ab</span>, wenn er dir nicht gefällt.",
+        sprache: "de"
+    },
+    {
+        titel: "ja",
+        untertitel: "(ja:)",
+        bedeutungen: "evet",
+        sprache: "de"
+    },
+
+
+    //tr
+
+    {
+        titel: "abece",
+        untertitel: "(a:be:ce:), Substantiv",
+        bedeutungen: "das Abc, das Alphabet",
+        beispiele: "Türkçe latin <span class='vurgu'>abecesini</span> kullanır.",
+        sprache: "tr"
+    },
+    {
+        titel: "adam",
+        untertitel: "Substantiv, aus dem arabischen ādem",
+        bedeutungen: "der Mann, der Mensch",
+        beispiele: "O akıllı bir <span class='vurgu'>adam</span>. (Er ist ein kluger <span class='vurgu'>Mann</span>.)",
+        sprache: "tr"
+    },
+    {
+        titel: "alfabe",
+        untertitel: "(aɫfabe), Substantiv, von französisch alphabet",
+        bedeutungen: "das Abc, das Alphabet",
+        beispiele: "Türkçe latin <span class='vurgu'>alfabesini</span> kullanır.",
+        sprache: "tr"
+    },
+    {
+        titel: "düzey",
+        untertitel: "Substantiv",
+        bedeutungen: "das Niveau, die Stufe",
+        sprache: "tr"
+    },
+    {
+        titel: "er",
+        untertitel: "Substantiv",
+        bedeutungen: "der Mann",
+        sprache: "tr"
+    },
+    {
+        titel: "erkek",
+        untertitel: "Substantiv, -ği",
+        bedeutungen: "der Mann",
+        sprache: "tr"
+    },
+    {
+        titel: "erkek",
+        untertitel: "Adjektiv, -ği",
+        bedeutungen: "männlich",
+        beispiele: "<span class='vurgu'>erkek</span> köpek (<span class='vurgu'>männlicher</span> Hund)",
+        sprache: "tr"
+    },
+    {
+        titel: "evet",
+        bedeutungen: "ja",
+        sprache: "tr"
+    },
+    {
+        titel: "hava",
+        untertitel: "Substantiv, aus dem arabischen hawā'",
+        bedeutungen: "1. die Luft; 2. das Wetter; 3. der Himmel; 4. das Klima",
+        sprache: "tr"
+    },
+    {
+        titel: "hava kirliliği",
+        untertitel: "Substantiv, von hava und kirlilik",
+        bedeutungen: "anlam",
+        sprache: "tr"
+    },
+    {
+        titel: "ocak",
+        untertitel: "Substantiv, -ğı",
+        bedeutungen: "1. der Herd, der Kocher; 2. der Dezember",
+        sprache: "tr"
+    },
+    {
+        titel: "sipariş",
+        untertitel: "(sipa:riş), Substantiv, von persisch sipāriş",
+        bedeutungen: "der Auftrag, die Bestellung",
+        sprache: "tr"
+    },
+    {
+        titel: "Türkçe",
+        untertitel: "Substantiv",
+        bedeutungen: "das Türkisch, die türkische Sprache",
+        beispiele: "Sen iyi <span class='vurgu'>Türkçe</span> konuşuyorsun. (Du sprichst gut <span class='vurgu'>Türkisch</span>.)",
+        sprache: "tr"
+    },
+    {
+        titel: "Türkiye",
+        untertitel: "Substantiv, -'yi",
+        bedeutungen: "die Türkei",
+        beispiele: "<span class='vurgu'>Türkiye</span>'den geldim. (Ich bin aus <span class='vurgu'>der Türkei</span> gekommt.)",
+        sprache: "tr"
+    },
+    {
+        titel: "yılan balığı",
+        untertitel: "Substantiv, von yılan und balık",
+        bedeutungen: "der Aal",
+        beispiele: "O çok büyük bir <span class='vurgu'>yılan balığı</span> yakalamıştı.",
+        sprache: "tr"
+    }
+
+
+
+
+    /*{
+        titel: "başlık",
+        untertitel: "ayrıntı",
+        bedeutungen: "anlam",
+        beispiele: "örnek",
+        sprache: "dilKodu"
+    }*/
+];
+
+createList(datas);
+
+function createList(datas) {
+    var list = document.getElementById("words");
+    
+    for (var i = 0; i < datas.length; i++) {
+        var data = datas[i];
+        var li = document.createElement("li");
+        li.className = "word";
+
+        var title = document.createElement("p");
+        if (data.sprache === "de") {
+            title.className = "titel";
+        }
+        else {
+            title.className = "baslik";
+        }
+        title.textContent = data.titel;
+
+        var subtitle = document.createElement("span");
+        subtitle.className = "untertitel";
+        subtitle.textContent = data.untertitel;
+
+        var details = document.createElement("div");
+        details.className = "details";
+
+        details.appendChild(title);
+        details.appendChild(subtitle);
+
+        var content = document.createElement("div");
+        content.className = "inhalt";
+
+        var meaningsTitle = document.createElement("p");
+        meaningsTitle.textContent = "Bedeutungen";
+
+        var meanings = document.createElement("span");
+        meanings.textContent = data.bedeutungen;
+
+        content.appendChild(meaningsTitle);
+        content.appendChild(meanings);
+
+        li.appendChild(details);
+        li.appendChild(content);
+
+        if (data.beispiele) {
+            var examples = document.createElement("div");
+            examples.className = "beispiele";
+
+            var examplesTitle = document.createElement("p");
+            examplesTitle.textContent = "Beispiele";
+            
+            var examplesContent = document.createElement("span");
+            examplesContent.innerHTML = data.beispiele;
+
+            examples.appendChild(examplesTitle);
+            examples.appendChild(examplesContent);
+
+            li.appendChild(examples);
+        }
+
+        list.appendChild(li);
+
+
+        var input = document.getElementById("input").value.toLowerCase();
+    var list = document.getElementById("words");
+    var words = list.getElementsByClassName("word");
+
+    for (var i = 0; i < words.length; i++) {
+        var word = words[i].getElementsByClassName("details")[0].getElementsByTagName("p")[0].innerText.toLowerCase();
+        if (input.length > 1 && word.includes(input)) {
+            words[i].style.display = "block";
+        } else {
+            words[i].style.display = "none";
+        }
+    }
+    }
+}
+
+function search() {
+    var input = document.getElementById("input").value.toLowerCase();
+    var list = document.getElementById("words");
+    var words = list.getElementsByClassName("word");
+
+    for (var i = 0; i < words.length; i++) {
+        var word = words[i].getElementsByClassName("details")[0].getElementsByTagName("p")[0].innerText.toLowerCase();
+        if (input === "@"){
+            words[i].style.display = "block";
+        }
+        else if (input.length > 1 && word.includes(input)) {
+            words[i].style.display = "block";
+        } else {
+            words[i].style.display = "none";
+        }
+    }
+}
